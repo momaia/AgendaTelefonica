e51e4df82fa12533a8c36ad9d2df1a6418d5f58d
Mateus Maia <maiamateusotavio@gmail.com>
2023-04-03 21:30:43 -0300

    Criacao do diretorio Models e configuracao da base de dados

---
+++ AgendaTelefonica/Agenda.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AgendaTelefonica;
+
+public class Agenda
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Nome")]
+    public string Nome { get; set; }
+    public string Telefone { get; set; }
+    public string telefoneCasa { get; set; } = null!;
+    public string telefoneTrabalho { get; set; } = null!;
+    public string telefoneOutro { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string EmailCasa { get; set; } = null!;
+    public string EmailTrabalho { get; set; } = null!;
+    public string EmailOutro { get; set; } = null!;
+    public string Empresa { get; set; } = null!;
+    public string Endereço { get; set; } = null!;
+}
